4a99c3af01f37f07f870aefcfd2f69cfbcad4c9f
s3miOtics <colby.kauk@gmail.com>
2024-02-06 00:07:29 -0800

    carousel component structure, basic styling, adding transition effect

--- vue3_carousel/src/App.vue
+++ vue3_carousel/src/App.vue
@@ -1,11 +1,26 @@
 <template>
- <h1>Hello World</h1>
+  <carousel :slides="slides"></carousel>
 </template>
 
 <script>
+
+import Carousel from "./components/carousel/Carousel.vue";
 export default {
   name: 'App',
+  components: { Carousel },
+  data: () => ({
+    slides: [ 
+      require("@/assets/home.png"),
+      require('@/assets/new_job.png'),
+      require('@/assets/job_details.png'),
+      require('@/assets/schedule_job.png')
+    ]
+  })
 }
+
+
 </script>
 
-<style></style>
+<style>
+
+</style>
---
+++ vue3_carousel/src/components/carousel/CarouselItem.vue
@@ -0,0 +1,40 @@
+<template>
+<transition name="slide-in">
+  <div class="carousel-item" v-show="currentSlide === index">
+    <img :src="slide" >
+  </div>
+</transition>
+
+</template>
+
+
+<script>
+export default {
+    props: ["slide", "currentSlide", "index"],
+}
+</script>
+
+<style>
+.carousel-item {
+  position: absolute; 
+  inset: 0;
+}
+
+.slide-in-enter-active,
+.slide-in-leave-active {
+  transition: all 1s ease-in-out;
+}
+
+.slide-in-enter-from {
+  transform: translateX(-100%)
+}
+
+.slide-in-leave-to {
+  transform: translateX(100%)
+}
+
+img { 
+  height: 400px;
+  width: 900px;
+}
+</style>
